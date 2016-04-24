e0a1aa0d273f2b48a91534c4e2d9fa8ff8da417b
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2016-04-24 15:44:48 +0300

    update for data-reconstructor

--- src/ModelReconstructor.php
+++ src/ModelReconstructor.php
@@ -3,6 +3,7 @@
 namespace RunetId\ApiClient;
 
 use Ruvents\DataReconstructor\DataReconstructor;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Class ModelReconstructor
@@ -13,32 +14,38 @@ class ModelReconstructor extends DataReconstructor
     /**
      * @inheritdoc
      */
-    protected $options = [
-        'map' => [
-            'user' => [
-                'CreationTime' => 'DateTime',
-                'Photo' => 'user_photo',
-                'Work' => 'user_work',
-                'Status' => 'user_status',
-            ],
-            'user_work' => [
-                'Company' => 'user_work_company',
-            ],
-            'user_status' => [
-                'UpdateTime' => 'DateTime',
-            ],
-        ],
-        'model_classes' => [
-            'error' => 'RunetId\ApiClient\Model\Error',
-            'user' => 'RunetId\ApiClient\Model\User',
-            'user_work_company' => 'RunetId\ApiClient\Model\User\Company',
-            'user_photo' => 'RunetId\ApiClient\Model\User\Photo',
-            'user_status' => 'RunetId\ApiClient\Model\User\Status',
-            'user_work' => 'RunetId\ApiClient\Model\User\Work',
-            'prof_interest' => 'RunetId\ApiClient\Model\ProfInterest',
-            'event' => 'RunetId\ApiClient\Model\Event',
-        ],
-    ];
+    protected function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver
+            ->setDefaults([
+                'map' => [
+                    'user' => [
+                        'CreationTime' => 'DateTime',
+                        'Photo' => 'user_photo',
+                        'Work' => 'user_work',
+                        'Status' => 'user_status',
+                    ],
+                    'user_work' => [
+                        'Company' => 'user_work_company',
+                    ],
+                    'user_status' => [
+                        'UpdateTime' => 'DateTime',
+                    ],
+                ],
+                'model_classes' => [
+                    'error' => 'RunetId\ApiClient\Model\Error',
+                    'user' => 'RunetId\ApiClient\Model\User',
+                    'user_work_company' => 'RunetId\ApiClient\Model\User\Company',
+                    'user_photo' => 'RunetId\ApiClient\Model\User\Photo',
+                    'user_status' => 'RunetId\ApiClient\Model\User\Status',
+                    'user_work' => 'RunetId\ApiClient\Model\User\Work',
+                    'prof_interest' => 'RunetId\ApiClient\Model\ProfInterest',
+                    'event' => 'RunetId\ApiClient\Model\Event',
+                ],
+            ]);
+    }
 
     /**
      * @inheritdoc
