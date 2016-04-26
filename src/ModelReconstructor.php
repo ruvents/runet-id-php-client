505d16ec6b5041c7f48e7010c2a38980a5a23953
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2016-04-26 11:30:52 +0300

    update

--- src/ModelReconstructor.php
+++ src/ModelReconstructor.php
@@ -3,6 +3,7 @@
 namespace RunetId\ApiClient;
 
 use Ruvents\DataReconstructor\DataReconstructor;
+use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -10,6 +11,46 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class ModelReconstructor extends DataReconstructor
 {
+    protected static $defaults = [
+        'map' => [
+            'user' => [
+                'CreationTime' => 'DateTime',
+                'Photo' => 'user_photo',
+                'Work' => 'user_work',
+                'Status' => 'user_status',
+            ],
+            'user_work' => [
+                'Company' => 'user_work_company',
+            ],
+            'user_status' => [
+                'UpdateTime' => 'DateTime',
+            ],
+            'section' => [
+                'Start' => 'DateTime',
+                'End' => 'DateTime',
+                'UpdateTime' => 'DateTime',
+                'Halls' => 'section_hall[]',
+            ],
+            'section_report' => [
+                'User' => 'user',
+                'UpdateTime' => 'DateTime',
+            ],
+        ],
+        'model_classes' => [
+            'error' => 'RunetId\ApiClient\Model\Error',
+            'user' => 'RunetId\ApiClient\Model\User',
+            'user_work_company' => 'RunetId\ApiClient\Model\User\Company',
+            'user_photo' => 'RunetId\ApiClient\Model\User\Photo',
+            'user_status' => 'RunetId\ApiClient\Model\User\Status',
+            'user_work' => 'RunetId\ApiClient\Model\User\Work',
+            'prof_interest' => 'RunetId\ApiClient\Model\ProfInterest',
+            'event' => 'RunetId\ApiClient\Model\Event',
+            'section' => 'RunetId\ApiClient\Model\Section',
+            'section_hall' => 'RunetId\ApiClient\Model\Section\Hall',
+            'section_report' => 'RunetId\ApiClient\Model\Section\Report',
+        ],
+    ];
+
     /**
      * @inheritdoc
      */
@@ -18,46 +59,11 @@ class ModelReconstructor extends DataReconstructor
         parent::configureOptions($resolver);
 
         $resolver
-            ->setDefaults([
-                'map' => [
-                    'user' => [
-                        'CreationTime' => 'DateTime',
-                        'Photo' => 'user_photo',
-                        'Work' => 'user_work',
-                        'Status' => 'user_status',
-                    ],
-                    'user_work' => [
-                        'Company' => 'user_work_company',
-                    ],
-                    'user_status' => [
-                        'UpdateTime' => 'DateTime',
-                    ],
-                    'section' => [
-                        'Start' => 'DateTime',
-                        'End' => 'DateTime',
-                        'UpdateTime' => 'DateTime',
-                        'Halls' => 'section_hall[]',
-                    ],
-                    'section_report' => [
-                        'User' => 'user',
-                        'UpdateTime' => 'DateTime',
-                    ],
-                ],
-                'model_classes' => [
-                    'error' => 'RunetId\ApiClient\Model\Error',
-                    'user' => 'RunetId\ApiClient\Model\User',
-                    'user_work_company' => 'RunetId\ApiClient\Model\User\Company',
-                    'user_photo' => 'RunetId\ApiClient\Model\User\Photo',
-                    'user_status' => 'RunetId\ApiClient\Model\User\Status',
-                    'user_work' => 'RunetId\ApiClient\Model\User\Work',
-                    'prof_interest' => 'RunetId\ApiClient\Model\ProfInterest',
-                    'event' => 'RunetId\ApiClient\Model\Event',
-                    'section' => 'RunetId\ApiClient\Model\Section',
-                    'section_hall' => 'RunetId\ApiClient\Model\Section\Hall',
-                    'section_report' => 'RunetId\ApiClient\Model\Section\Report',
-                ],
-            ])
-            ->setAllowedTypes('model_classes', 'array');
+            ->setDefault('model_classes', [])
+            ->setAllowedTypes('model_classes', 'array')
+            ->setNormalizer('model_classes', function (Options $options, $value) {
+                return array_replace(self::$defaults['model_classes'], $value);
+            });
     }
 
     /**
