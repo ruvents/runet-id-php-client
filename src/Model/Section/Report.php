b9b234c7894f0b2d8766eab26ac92cfa8e9b2024
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2016-04-26 18:03:01 +0300

    update

--- src/Model/Section/Report.php
+++ src/Model/Section/Report.php
@@ -9,6 +9,16 @@ use RunetId\ApiClient\Model\User;
  */
 class Report
 {
+    const ROLE_ORGANIZER = 1;
+
+    const ROLE_HOST = 2;
+
+    const ROLE_REPORTER = 3;
+
+    const ROLE_ORGANIZER_HOST = 4;
+
+    const ROLE_ROUND_TABLE = 5;
+
     /**
      * @var int
      */
@@ -70,10 +80,62 @@ class Report
     public $Deleted;
 
     /**
+     * @return array
+     */
+    public static function getRoles()
+    {
+        return [
+            self::ROLE_ORGANIZER,
+            self::ROLE_HOST,
+            self::ROLE_REPORTER,
+            self::ROLE_ORGANIZER_HOST,
+            self::ROLE_ROUND_TABLE,
+        ];
+    }
+
+    /**
      * @return string
      */
     public function __toString()
     {
         return (string)$this->Title;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRoleHost()
+    {
+        return in_array($this->SectionRoleId, [
+            self::ROLE_HOST,
+            self::ROLE_ORGANIZER_HOST,
+        ]);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRoleOrganizer()
+    {
+        return in_array($this->SectionRoleId, [
+            self::ROLE_ORGANIZER,
+            self::ROLE_ORGANIZER_HOST,
+        ]);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRoleReporter()
+    {
+        return $this->SectionRoleId == self::ROLE_REPORTER;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRoleRoundTable()
+    {
+        return $this->SectionRoleId == self::ROLE_ROUND_TABLE;
     }
 }
