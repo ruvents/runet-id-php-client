a73639bd36b8048659ac9a1147a34be5f144c106
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2016-05-27 15:24:18 +0300

    update

--- src/Model/User/Photo.php
+++ src/Model/User/Photo.php
@@ -21,4 +21,20 @@ class Photo
      * @var string
      */
     public $Large;
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->Large;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasPhoto()
+    {
+        return '/files/photo/nophoto_50.png' !== parse_url($this->Small, PHP_URL_PATH);
+    }
 }
