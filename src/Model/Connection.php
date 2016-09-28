f918224925005f7ee3029253009df7b0fcea7170
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2016-09-28 16:32:42 +0300

    Индексация Users по RunetId

--- src/Model/Connection.php
+++ src/Model/Connection.php
@@ -82,6 +82,13 @@ class Connection implements ReconstructableInterface
     {
         $this->Start = new DateTime($data['Date'].' '.$data['Time']);
         $this->End = (clone $this->Start)->add(new DateInterval('PT'.$data['Place']['ReservationTime'].'M'));
+
+        $users = [];
+        foreach ($data['Users'] as $response) {
+            $users[$response['User']['RunetId']] = $response;
+        }
+        $data['Users'] = $users;
+
         unset($data['Date'], $data['Time']);
     }
 }
