6cd9784db73e5f3565ae36769bbb110de3c0199e
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2016-10-20 23:41:57 +0300

    Поправил @throws аннотации
    (cherry picked from commit acc6c35)

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -15,6 +15,8 @@ use Ruvents\HttpClient\Request\Request;
 use Ruvents\HttpClient\Request\Uri;
 use Ruvents\HttpClient\Response\Response;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\Exception as ResolverException;
+use Ruvents\HttpClient\Exception as HttpException;
 
 /**
  * Class ApiClient
@@ -34,12 +36,12 @@ class ApiClient
     /**
      * @param array                  $options
      * @param DataReconstructor|null $modelReconstructor
-     * @throws \Symfony\Component\OptionsResolver\Exception\AccessException
-     * @throws \Symfony\Component\OptionsResolver\Exception\InvalidOptionsException
-     * @throws \Symfony\Component\OptionsResolver\Exception\MissingOptionsException
-     * @throws \Symfony\Component\OptionsResolver\Exception\NoSuchOptionException
-     * @throws \Symfony\Component\OptionsResolver\Exception\OptionDefinitionException
-     * @throws \Symfony\Component\OptionsResolver\Exception\UndefinedOptionsException
+     * @throws ResolverException\AccessException
+     * @throws ResolverException\InvalidOptionsException
+     * @throws ResolverException\MissingOptionsException
+     * @throws ResolverException\NoSuchOptionException
+     * @throws ResolverException\OptionDefinitionException
+     * @throws ResolverException\UndefinedOptionsException
      */
     public function __construct(array $options, DataReconstructor $modelReconstructor = null)
     {
@@ -93,9 +95,9 @@ class ApiClient
      * @param array  $data
      * @param array  $headers
      * @return Response
-     * @throws \Ruvents\HttpClient\Exception\RuntimeException
-     * @throws \Ruvents\HttpClient\Exception\CurlException
-     * @throws \Ruvents\HttpClient\Exception\InvalidArgumentException
+     * @throws HttpException\RuntimeException
+     * @throws HttpException\CurlException
+     * @throws HttpException\InvalidArgumentException
      */
     public function get($path, array $data = array(), array $headers = array())
     {
@@ -111,9 +113,9 @@ class ApiClient
      * @param array             $headers
      * @param array             $files
      * @return Response
-     * @throws \Ruvents\HttpClient\Exception\RuntimeException
-     * @throws \Ruvents\HttpClient\Exception\CurlException
-     * @throws \Ruvents\HttpClient\Exception\InvalidArgumentException
+     * @throws HttpException\RuntimeException
+     * @throws HttpException\CurlException
+     * @throws HttpException\InvalidArgumentException
      */
     public function post($path, array $query = array(), $data = null, array $headers = array(), array $files = array())
     {
@@ -174,8 +176,8 @@ class ApiClient
 
     /**
      * @param OptionsResolver $resolver
-     * @throws \Symfony\Component\OptionsResolver\Exception\AccessException
-     * @throws \Symfony\Component\OptionsResolver\Exception\UndefinedOptionsException
+     * @throws ResolverException\AccessException
+     * @throws ResolverException\UndefinedOptionsException
      */
     protected function configureOptions(OptionsResolver $resolver)
     {
@@ -206,11 +208,11 @@ class ApiClient
      * @param array             $headers
      * @param array             $files
      * @return Request
-     * @throws \Ruvents\HttpClient\Exception\InvalidArgumentException
+     * @throws HttpException\InvalidArgumentException
      */
-    protected function createRequest($path, array $query = array(), $data = null, array $headers = array(), array $files = array()
-    )
-    {
+    protected function createRequest(
+        $path, array $query = array(), $data = null, array $headers = array(), array $files = array()
+    ) {
         $query = array_merge(array(
             'ApiKey' => $this->options['key'],
             'Hash' => $this->generateHash($this->options['key'], $this->options['secret']),
@@ -231,8 +233,8 @@ class ApiClient
      * @param string  $method
      * @param Request $request
      * @return Response
-     * @throws \Ruvents\HttpClient\Exception\RuntimeException
-     * @throws \Ruvents\HttpClient\Exception\CurlException
+     * @throws HttpException\RuntimeException
+     * @throws HttpException\CurlException
      */
     protected function send($method, Request $request)
     {
