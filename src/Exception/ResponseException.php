10049d932f299c6381bb9200e03a727ef0afa967
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2016-10-20 23:44:42 +0300

    Возможность добавлять в ResponseException объект Response (удобно при catch)

--- src/Exception/ResponseException.php
+++ src/Exception/ResponseException.php
@@ -2,9 +2,33 @@
 
 namespace RunetId\ApiClient\Exception;
 
+use Exception;
+use Ruvents\HttpClient\Response\Response;
+
 /**
  * Class ResponseException
  */
 class ResponseException extends ApiException implements ApiExceptionInterface
 {
+    /**
+     * @var null|Response
+     */
+    private $response;
+
+    /**
+     * {@inheritdoc}
+     * @param null|Response $response
+     */
+    public function __construct($message = '', $code = 0, Exception $previous = null, Response $response = null)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * @return null|Response
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
 }
