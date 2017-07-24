766c9b29d829a09e53111caa28b7451475fc98bc
Valentin <udaltsov.valentin@gmail.com>
2017-07-24 16:54:53 +0300

    Фасады / Event / Метод register

--- lib/Facade/EventFacade.php
+++ lib/Facade/EventFacade.php
@@ -38,6 +38,21 @@ class EventFacade extends AbstractFacade
     }
 
     /**
+     * @param int|UserRunetIdInterface $runetId
+     * @param int|RoleIdInterface      $roleId
+     * @param array                    $context
+     *
+     * @return true|array
+     */
+    public function register($runetId, $roleId, array $context = [])
+    {
+        return $this->requestPost($context, '/event/register', [
+            'RunetId' => ArgHelper::getUserRunetId($runetId),
+            'RoleId' => ArgHelper::getEventRoleId($roleId),
+        ]);
+    }
+
+    /**
      * @param array $context
      *
      * @return array|Role[]
