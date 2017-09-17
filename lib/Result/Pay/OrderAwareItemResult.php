80878cbe631d7bf8cacc41fba3d5401d96d249c8
Valentin <udaltsov.valentin@gmail.com>
2017-09-17 12:47:07 +0300

    Result / OrderAwareItemResult

---
+++ lib/Result/Pay/OrderAwareItemResult.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace RunetId\ApiClient\Result\Pay;
+
+class OrderAwareItemResult extends ItemResult
+{
+    /**
+     * @var null|OrderResult
+     */
+    private $order;
+
+    /**
+     * @param array            $result
+     * @param null|OrderResult $order
+     */
+    public function __construct(array $result, OrderResult $order = null)
+    {
+        parent::__construct($result);
+        $this->order = $order;
+    }
+
+    /**
+     * @return null|OrderResult
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+}
--- lib/Result/Pay/OrderResult.php
+++ lib/Result/Pay/OrderResult.php
@@ -6,19 +6,19 @@ use RunetId\ApiClient\ArgumentHelper\PayOrderIdInterface;
 use RunetId\ApiClient\Result\AbstractResult;
 
 /**
- * @property int          $OrderId
- * @property string       $CreationTime
- * @property string       $Number
- * @property bool         $Paid
- * @property string       $Url
- * @property ItemResult[] $Items
+ * @property int                    $OrderId
+ * @property string                 $CreationTime
+ * @property string                 $Number
+ * @property bool                   $Paid
+ * @property string                 $Url
+ * @property OrderAwareItemResult[] $Items
  */
 class OrderResult extends AbstractResult implements PayOrderIdInterface
 {
     public function __construct(array $result)
     {
         $result['Items'] = array_map(function (array $result) {
-            return new ItemResult($result, $this);
+            return new OrderAwareItemResult($result, $this);
         }, $result['Items']);
 
         parent::__construct($result);
