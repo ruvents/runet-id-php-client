466463b50912c8d8601f3f0547e6084e4b6fca6b
Valentin <udaltsov.valentin@gmail.com>
2018-01-14 17:31:14 +0300

    Имплементация итератора на AbstractResult, подробные исключения.

--- Result/AbstractResult.php
+++ Result/AbstractResult.php
@@ -5,7 +5,7 @@ namespace RunetId\Client\Result;
 /**
  * @internal
  */
-abstract class AbstractResult
+abstract class AbstractResult implements \IteratorAggregate
 {
     private $data;
 
@@ -15,23 +15,64 @@ abstract class AbstractResult
     }
 
     /**
-     * @param string $offset
-     *
-     * @return null|mixed
-     */
-    public function __get($offset)
-    {
-        return isset($this->data[$offset]) ? $this->data[$offset] : null;
-    }
-
-    /**
-     * @param string $offset
+     * @param int|string $offset
      *
      * @return bool
      */
     public function __isset($offset)
     {
         return isset($this->data[$offset]);
+    }
+
+    /**
+     * @param int|string $offset
+     *
+     * @throws \OutOfBoundsException
+     *
+     * @return mixed
+     */
+    public function __get($offset)
+    {
+        if (!$this->exists($offset)) {
+            $message = sprintf('Offset "%s" does not exist in the result array.', $offset);
+
+            if ([] === $this->data) {
+                $message .= ' The result array is empty.';
+            } elseif (1 === count($this->data)) {
+                $message .= sprintf(' "%s" offset is available.', key($this->data));
+            } else {
+                $message .= sprintf(' "%s" offsets are available.', implode('", "', array_keys($this->data)));
+            }
+
+            throw new \OutOfBoundsException($message);
+        }
+
+        return $this->data[$offset];
+    }
+
+    /**
+     * @param int|string $offset
+     * @param mixed      $value
+     *
+     * @throws \LogicException
+     *
+     * @return void
+     */
+    public function __set($offset, $value)
+    {
+        throw new \LogicException('Result is immutable.');
+    }
+
+    /**
+     * @param int|string $offset
+     *
+     * @throws \LogicException
+     *
+     * @return void
+     */
+    public function __unset($offset)
+    {
+        throw new \LogicException('Result is immutable.');
     }
 
     /**
@@ -43,12 +84,20 @@ abstract class AbstractResult
     }
 
     /**
-     * @param string $offset
+     * @param int|string $offset
      *
      * @return bool
      */
     public function exists($offset)
     {
         return array_key_exists($offset, $this->data);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->data);
     }
 }
--- Test/Result/AbstractResultTest.php
+++ Test/Result/AbstractResultTest.php
@@ -7,19 +7,75 @@ use RunetId\Client\Test\Fixtures\Result\TestResult;
 
 final class AbstractResultTest extends TestCase
 {
-    public function test()
+    public function testMagicPropertyAccess()
     {
         $result = new TestResult(['Id' => 1, 'Name' => null]);
 
-        $this->assertSame(1, $result->Id);
-        $this->assertNull($result->Collection);
-
-        $this->assertTrue(isset($result->Id));
-        $this->assertFalse(isset($result->Name));
-        $this->assertFalse(isset($result->Collection));
-
         $this->assertTrue($result->exists('Id'));
+        $this->assertTrue(isset($result->Id));
+        $this->assertNotEmpty($result->Id);
+        $this->assertSame(1, $result->Id);
+
         $this->assertTrue($result->exists('Name'));
+        $this->assertFalse(isset($result->Name));
+        $this->assertEmpty($result->Name);
+        $this->assertNull($result->Name);
+
         $this->assertFalse($result->exists('Collection'));
+        $this->assertFalse(isset($result->Collection));
+        $this->assertEmpty($result->Collection);
+    }
+
+    public function testIterator()
+    {
+        $data = ['Id' => 1, 'Name' => null];
+        $this->assertSame($data, iterator_to_array(new TestResult($data)));
+    }
+
+    /**
+     * @expectedException \OutOfBoundsException
+     * @expectedExceptionMessage Offset "Id" does not exist in the result array. The result array is empty.
+     */
+    public function testMagicGetOffsetFromEmptyException()
+    {
+        (new TestResult([]))->Id;
+    }
+
+    /**
+     * @expectedException \OutOfBoundsException
+     * @expectedExceptionMessage Offset "Id" does not exist in the result array. "Collection" offset is available.
+     */
+    public function testMagicGetOffsetFromSingleOffsetException()
+    {
+        (new TestResult(['Collection' => []]))->Id;
+    }
+
+    /**
+     * @expectedException \OutOfBoundsException
+     * @expectedExceptionMessage Offset "Id" does not exist in the result array. "Collection", "Name" offsets are available.
+     */
+    public function testMagicGetOffsetFromMultipleOffsetsException()
+    {
+        (new TestResult(['Collection' => [], 'Name' => 'name']))->Id;
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Result is immutable.
+     */
+    public function testMagicSetException()
+    {
+        $result = new TestResult([]);
+        $result->Id = 1;
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Result is immutable.
+     */
+    public function testMagicUnsetException()
+    {
+        $result = new TestResult([]);
+        unset($result->Id);
     }
 }
