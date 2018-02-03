dae6ad5e5aad53929d8a57aa1d020754d41c1efb
Valentin <udaltsov.valentin@gmail.com>
2018-02-03 21:40:30 +0300

    Добавлен constants.php с полезными константами.

---
+++ constants.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace RunetId\Event\Roles {
+    /**
+     * Участник.
+     */
+    const PARTICIPANT = 1;
+
+    /**
+     * СМИ.
+     */
+    const MEDIA = 2;
+
+    /**
+     * Докладчик.
+     */
+    const SPEAKER = 3;
+
+    /**
+     * Партнёр.
+     */
+    const PARTNER = 5;
+
+    /**
+     * Организатор.
+     */
+    const ORGANIZER = 6;
+
+    /**
+     * Виртуальный участник.
+     */
+    const VIRTUAL = 24;
+
+    /**
+     * Видеоучастник.
+     */
+    const VIDEO = 26;
+}
