25c67d3fa676ed05353c3919b64908e3964c27c2
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2018-02-18 20:27:10 +0300

    Поправлен тип аргумента в Pay\CouponEndpoint::setOwnerRunetId().

--- src/Endpoint/Pay/CouponEndpoint.php
+++ src/Endpoint/Pay/CouponEndpoint.php
@@ -7,7 +7,7 @@ use RunetId\Client\Result\Pay\CouponResult;
 
 /**
  * @method $this        setCouponCode(string $couponCode)
- * @method $this        setOwnerRunetId(array $ownerRunetId)
+ * @method $this        setOwnerRunetId(int $ownerRunetId)
  * @method $this        setPayerRunetId(int $payerRunetId)
  * @method $this        setProductId(int $productId)
  * @method CouponResult getResult()
